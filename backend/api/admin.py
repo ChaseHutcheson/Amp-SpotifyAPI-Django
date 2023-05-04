0fbf797832face54c1d3386f4133346ba40865b2
Chase Hutcheson <ChaseHutcheson2006@gmail.com>
2023-05-04 11:36:17 -0400

    Augh

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,7 +2,5 @@ from django.contrib import admin
 from .models import SpotifyToken
 
 # Register your models here.
-class SpotifyTokenAdmin(admin.ModelAdmin):
-    list_display = ('session_id', 'user_name')  # Fields to be displayed on the change list page
 
-admin.site.register(SpotifyToken, SpotifyTokenAdmin)
+admin.site.register(SpotifyToken)
