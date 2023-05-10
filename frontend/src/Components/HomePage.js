2c073dfde6bff84190f5873c9a9c97e378952898
Chase Hutcheson <ChaseHutcheson2006@gmail.com>
2023-05-10 13:09:48 -0400

    Invalid Access Token keeps happening

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -5,135 +5,112 @@ import SpotifyPlayer from 'react-spotify-web-playback';
 import WebPlayback from './Playback';
 
 const HomePage = () => {
-    const [myData, setMyData] = useState([]);
-    const [playlistData, setPlaylistData] = useState([]);
-    const [dataRetreieved, setDataRetrieved] = useState(false);
-    const [token, setToken] = useState(null);
-    const [player, setPlayer] = useState(undefined);
-    const [is_paused, setPaused] = useState(false);
-    const [is_active, setActive] = useState(false);
+  const [myData, setMyData] = useState([]);
+  const [playlistData, setPlaylistData] = useState([]);
+  const [token, setToken] = useState(null);
+  const [player, setPlayer] = useState(undefined);
+  const [is_paused, setPaused] = useState(false);
+  const [is_active, setActive] = useState(false);
 
-    function getCookie(name) {
-      let cookieValue = null;
-      if (document.cookie && document.cookie !== '') {
-          const cookies = document.cookie.split(';');
-          for (let i = 0; i < cookies.length; i++) {
-              const cookie = cookies[i].trim();
-              // Does this cookie string begin with the name we want?
-              if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                  break;
-              }
-          }
-      }
-      return cookieValue;
-    }
-    
-    const fetchUserData = () => {
-        fetch("http://127.0.0.1:8000/api/spotify-callback", {
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': getCookie('csrftoken') //getCookie function retrieves the CSRF token from the cookie
-          }
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const UserData = await (
+        await fetch("http://127.0.0.1:8000/api/spotify-callback"
+        ).then(response => {
+          return response.json();
         }).then(response => {
-            return response.json();
-          })
-          .then(res => {
-            setMyData(res);
-            setDataRetrieved(!dataRetreieved)
-            setToken(myData.SENSETIVE?.access_token)
-            console.log(myData)
-            console.log(token)
-          }).catch(console.error);;
-      }
+          setToken(response.SENSETIVE.access_token)
+        })
+      )
+      setMyData(UserData)
+    };
 
-      const fetchUserPlaylists = () => {
-        // use this => https://api.spotify.com/v1/playlists/{playlist_id}/tracks
-        fetch(myData.PLAYLIST_SONGS?.items.tracks.href, {
+    fetchUserData()
+  }, [])
+
+  useEffect(() => {
+    const fetchUserPlaylists = async () => {
+      const UserPlaylists = await (
+        await fetch('https://api.spotify.com/v1/playlists/3I0r3nEZzs8bRDo9lRlReL/tracks', {
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': "Bearer" + token
+            'Authorization': `Bearer ${token}`
           }
         }).then(response => {
           return response.json()
-        }).then(res => {
-          setPlaylistData(res)
-          console.log(res)
         })
-      }
+      )
+      setPlaylistData(UserPlaylists)
+    }
 
-      useEffect(() => {
-        fetchUserData()
-        setToken(myData.SENSETIVE?.access_token)
-        console.log(myData)
-        console.log(token)
-        fetchUserPlaylists()
-        console.log(playlistData)
-      }, [])
+    if (token) {
+      fetchUserPlaylists()
+    }
+  }, [token])
   
-      return (
-        <body>
-          <header class="bg-black py-3">
-            <div class="app-container">
-              <div class="flex justify-between items-center text-white">
-                <figure>
-                  <a href="">
-                    <h1 className='ml-2 font-bold text-lg'>AMP</h1>
-                  </a>
+  return (
+    <div>
+      <header class="bg-black py-3">
+        <div class="app-container">
+          <div class="flex justify-between items-center text-white">
+            <figure>
+              <a href="">
+                <h1 className='ml-2 font-bold text-lg'>AMP</h1>
+              </a>
+            </figure>
+
+            <div class="flex items-center">        
+              <div class="flex items-center gap-3 lg:gap-0 ml-4 relative">
+                <figure class="ml-8 cursor-pointer flex items-center gap-3 hover:text-hoverspt">
+                  <img class="inline-block h-8 w-8 lg:h-11 lg:w-11 rounded-full" alt="User Photo"></img>
+                  <span class="font-bold hidden lg:block"></span>
+                  <i class="bi bi-chevron-down text-xs hidden lg:block"></i>
+                  <button><i class="bi bi-list text-white text-2xl block lg:hidden"></i></button>
                 </figure>
-
-                <div class="flex items-center">        
-                  <div class="flex items-center gap-3 lg:gap-0 ml-4 relative">
-                    <figure class="ml-8 cursor-pointer flex items-center gap-3 hover:text-hoverspt">
-                      <img class="inline-block h-8 w-8 lg:h-11 lg:w-11 rounded-full" src={myData.PROFILE_DATA?.images[0].url} alt="User Photo"></img>
-                      <span class="font-bold hidden lg:block">{myData.PROFILE_DATA?.display_name}</span>
-                      <i class="bi bi-chevron-down text-xs hidden lg:block"></i>
-                      <button><i class="bi bi-list text-white text-2xl block lg:hidden"></i></button>
-                    </figure>
-                  </div>
-                </div>
               </div>
             </div>
-          </header>
+          </div>
+        </div>
+      </header>
 
-          <main>
-            <section class="flex flex-col items-center bg-pink h-97 md:h-98 lg:h-99 xl:h-100">
-              <div class="app-container">
-                <div class="text-center pt-5">
-                  <h1 class="font-bold text-2xl sm:text-4xl md:text-9xl md:leading-11 text-red">Try Amp Now!</h1>
-                </div>
-              </div>
-            </section>
-
-            <section 
-              class="h-480 md:h-600 bg-spotify-theme-moblie xl:bg-spotify-theme bg-banner-mobile xl:bg-banner bg-181% xl:bg-100% bg-purple-main bg-no-repeat flex items-center md:block"
-            >
-              <div class="app-container">
-                <div class='text-green-main text-center md:pt-124'>
-                  <h1 class="text-3xl mt-20 font-bold md:text-6xl">Is it music you want?</h1>
-                  <h2 class="font-medium mt-7 md:mt-11 mb-8 md:mb-11 md:text-2xl">Listen to the best releases of the moment.</h2>
-                  <div>
-                    <WebPlayback token={token}/>
-                  </div>
-                  <ul>
-                    
-                  </ul>
-                </div>
-              </div>
-            </section>
-          </main>
-
-          <footer class="bg-black fixed inset-x-0 bottom-0 py-20">
-            <div class="app-container">
-              <div class="text-white text-center">
-                <div>
-                  <h4>Super cool epic footer</h4>
-                </div>
-              </div>
+      <main>
+        <section class="flex flex-col items-center bg-pink h-97 md:h-98 lg:h-99 xl:h-100">
+          <div class="app-container">
+            <div class="text-center pt-5">
+              <h1 class="font-bold text-2xl sm:text-4xl md:text-9xl md:leading-11 text-red">Try Amp Now!</h1>
             </div>
-          </footer>
-        </body>
-      );
+          </div>
+        </section>
+
+        <section 
+          class="h-480 md:h-600 bg-spotify-theme-moblie xl:bg-spotify-theme bg-banner-mobile xl:bg-banner bg-181% xl:bg-100% bg-purple-main bg-no-repeat flex items-center md:block"
+        >
+          <div class="app-container">
+            <div class='text-green-main text-center md:pt-124'>
+              <h1 class="text-3xl mt-20 font-bold md:text-6xl">Is it music you want?</h1>
+              <h2 class="font-medium mt-7 md:mt-11 mb-8 md:mb-11 md:text-2xl">Listen to the best releases of the moment.</h2>
+              <div>
+                <WebPlayback token={token}/>
+              </div>
+              <ul>
+                
+              </ul>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      <footer class="bg-black fixed inset-x-0 bottom-0 py-20">
+        <div class="app-container">
+          <div class="text-white text-center">
+            <div>
+              <h4>Super cool epic footer</h4>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
     
 }
 
--- frontend/src/Components/Playback.js
+++ frontend/src/Components/Playback.js
@@ -1,61 +1,75 @@
 import React, { useState, useEffect } from 'react';
 
 function WebPlayback(props) {
+    const [token, setToken] = useState(false)
     const [player, setPlayer] = useState(undefined);
     const [is_paused, setPaused] = useState(false);
     const [is_active, setActive] = useState(false);
     const [current_track, setTrack] = useState(undefined);
 
-
-
     useEffect(() => {
+        // const script = document.createElement("script");
+        // script.src = "https://sdk.scdn.co/spotify-player.js";
+        // script.async = true;
+    
+        // document.body.appendChild(script);
+    
+        // window.onSpotifyWebPlaybackSDKReady = async () => {
+    
+        //     const player = new window.Spotify.Player({
+        //         name: 'Web Playback SDK',
+        //         getOAuthToken: cb => { cb(props.token); },
+        //         volume: 0.5
+        //     });
+    
+        //     setPlayer(player);
+    
+        //     player.addListener('ready', ({ device_id }) => {
+        //         console.log('Ready with Device ID', device_id);
+        //     });
+    
+        //     player.addListener('not_ready', ({ device_id }) => {
+        //         console.log('Device ID has gone offline', device_id);
+        //     });
+    
+    
+        //     player.connect();
 
-        const script = document.createElement("script");
-        script.src = "https://sdk.scdn.co/spotify-player.js";
-        script.async = true;
-    
-        document.body.appendChild(script);
-    
-        window.onSpotifyWebPlaybackSDKReady = () => {
-    
-            const player = new window.Spotify.Player({
-                name: 'Web Playback SDK',
-                getOAuthToken: cb => { cb(props.token); },
-                volume: 0.5
-            });
-    
-            setPlayer(player);
-    
-            player.addListener('ready', ({ device_id }) => {
-                console.log('Ready with Device ID', device_id);
-            });
-    
-            player.addListener('not_ready', ({ device_id }) => {
-                console.log('Device ID has gone offline', device_id);
-            });
-    
-    
-            player.connect();
+        //     player.addListener('player_state_changed', ( state => {
 
-            player.addListener('player_state_changed', ( state => {
-
-                if (!state) {
-                    return;
+        //         if (!state) {
+        //             return;
+        //         }
+            
+        //         setTrack(state.track_window.current_track);
+        //         setPaused(state.paused);
+            
+            
+        //         player.getCurrentState().then( state => { 
+        //             (!state)? setActive(false) : setActive(true) 
+        //         });
+            
+        //     }));
+            
+    
+        // };
+        const fetchUserData = async () => {
+            const UserPlaylists = await (
+                await fetch('https://api.spotify.com/v1/playlists/3I0r3nEZzs8bRDo9lRlReL/tracks', {
+                    headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
                 }
-            
-                setTrack(state.track_window.current_track);
-                setPaused(state.paused);
-            
-            
-                player.getCurrentState().then( state => { 
-                    (!state)? setActive(false) : setActive(true) 
-                });
-            
-            }));
-            
-    
-        };
-    }, []);
+                }).then(response => {
+                return response.json()
+                }).then(res => {
+                    console.log(res)
+                }
+                )
+            )
+          };
+          fetchUserData()
+    }, [token]);
 
     return (
         <>
